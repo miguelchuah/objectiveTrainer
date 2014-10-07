9ef36542250300d421e716cdeafb7ce1ab3f3635
miguelchuah <mapplemig@gmail.com>
2014-10-08 00:32:24 +0800

    initial commit

---
+++ objectiveTrainer/objectiveTrainer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  objectiveTrainer
+//
+//  Created by Miguel Nardy on 8/10/14.
+//  Copyright (c) 2014 tutorial. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
